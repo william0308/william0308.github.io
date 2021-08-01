049e8c42bff1f79fd009f00b45e4ec3c9a40df5b
william0308 <88258585+william0308@users.noreply.github.com>
2021-08-01 12:58:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function handle_click() {    
+    var music = document.getElementById('mymp3');     
+    if (music.paused){    
+        music.play();
+    }   
+};
+function y() {    
+    var music = document.getElementById('mmymp3');     
+    if (music.paused){    
+        music.play();
+    }   
+};
